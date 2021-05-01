8169b6605dbfe13912e8d08282c65326b4c5f033
iaconom821 <iaconom821@gmail.com>
2021-05-01 11:24:34 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Triangle
   # write code here
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def greater_than_zero
+    @side_3 > 0 && @side_2 > 0 && @side_1 > 0
+  end
+
+  def kind
+    if (@side_3 == @side_2 && @side_3 == @side_1) && self.greater_than_zero
+      :equilateral
+    elsif (@side_1 == @side_2 && (@side_1+@side_2>@side_3) || @side_2 == @side_3 && (@side_3+@side_2>@side_1)|| @side_1 == @side_3 && (@side_1+@side_3>@side_2)) && self.greater_than_zero
+      :isosceles
+    elsif ((@side_1+@side_2>@side_3) && (@side_3+@side_2>@side_1) && (@side_1+@side_3>@side_2)) && self.greater_than_zero
+      :scalene
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
+
+#binding.pry
